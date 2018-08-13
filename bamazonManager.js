def5107c514ecc13638a6b54d5aa0a4d7270c281
Georgy Baranov <floridavbv@gmail.com>
2018-08-13 17:23:49 -0600

    0.11b

--- bamazonManager.js
+++ bamazonManager.js
@@ -10,8 +10,41 @@ var connection = mysql.createConnection({
     database: db.database
 });
 
+
+function viewProductsForSale() {
+ connection.query('SELECT * FROM products', function(err, result) {
+     if (err) throw err;
+     console.log(result);
+ });
+connection.end();
+};
+
+function viewLowInventory() {
+
+};
+
+function addToInventory() {
+
+};
+
+function addNewProduct() {
+
+};
+
 inqurer.prompt([{
     name: 'selection',
     type: 'list',
-    message: 'What to do:'
-}])
+    message: 'What to do:',
+    choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product']
+}]).then((selected) => {
+    switch (selected.selection) {
+        case 'View Products for Sale': viewProductsForSale();
+        break;
+        case 'View Low Inventory': viewLowInventory();
+        break;
+        case 'Add to Inventory': addToInventory();
+        break;
+        case 'Add New Product': addNewProduct();
+        break;
+    }
+});
